22a23fffbfcf89f4afabaa69336efa7a97349396
Rodrigo Falco <rodrigofalco@gmail.com>
2015-09-09 21:04:17 -0300

    Try modifying tomcat opts.

--- run.sh
+++ run.sh
@@ -6,5 +6,5 @@
 if [ ! -f ${TOMCAT_CONFIGURATION_FLAG} ]; then
     /usergrid/create_tomcat_admin_user.sh
 fi
-
+JAVA_OPTS="-Djava.library.path=/usr/local/apr/lib -Djava.security.egd=file:/dev/./urandom -Djava.awt.headless=true -Xmx512m -XX:MaxPermSize=512m -XX:+UseConcMarkSweepGC"
 exec /usr/share/tomcat7/bin/catalina.sh run
